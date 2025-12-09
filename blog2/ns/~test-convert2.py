1b92ec484fc6d40853a6081908414d89e10bad1c
Donald Chitester <donaldchitester@gmail.com>
2025-12-09 14:06:26 -0700

    pybak

--- blog2/ns/~test-convert2.py
+++
@@ -1,5 +0,0 @@
-from pdf2csv.converter import convert
-
-dfs = convert("example.pdf", output_dir="./output", output_format="csv")
-for df in dfs:
-    df.to_csv("output.csv", index=False)  # Save each DataFrame to a CSV
--- blog2/ns/~test-convert3.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import tabula
-
-# Convert tables from 'input.pdf' to a list of DataFrames
-dfs = tabula.read_pdf("netspend-statement.pdf", pages="all", multiple_tables=True)
-# Save each DataFrame to a separate sheet in an Excel file
-
-print(dfs)
